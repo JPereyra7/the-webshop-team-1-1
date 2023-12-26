b4ac5ec130cb1a8961309c940f39c7dee2632d30
Amanda Valencia <amandaavalencia@hotmail.com>
2023-12-26 12:20:41 +0100

    created class with plants

---
+++ src/ts/products.ts
@@ -0,0 +1,10 @@
+export class Plant {
+    constructor(
+        public name: string,
+        public image: string,
+        public price: number,
+        public productInfo: string,
+        public plantId: number
+    ){}
+}
+
