0ef21e3ae0de18ad0e4b73171b01ecce92819e6d
Joel Pereyra <joelpereyra1993@gmail.com>
2023-12-28 13:03:14 +0100

    Added click event to a new page

--- src/ts/createHtml.ts
+++ src/ts/createHtml.ts
@@ -6,11 +6,9 @@ export function createHtmlForLandingpage() {
     for (let i = 0; i < plantList.length; i++) {
       const plant = plantList[i];
   
-      // Create a wrapper div for each plant item
       const plantItemDiv = document.createElement("div");
       plantItemDiv.classList.add("plant");
   
-      // Create an image element
       const imageElement = document.createElement("img");
       imageElement.src = plant.image;
       imageElement.alt = plant.plantName;
@@ -22,10 +20,15 @@ export function createHtmlForLandingpage() {
       const priceElement = document.createElement("p");
       priceElement.innerHTML = `${plant.price} kr`;
   
-      // Append image and name to the wrapper div
       plantItemDiv.appendChild(imageElement);
       plantItemDiv.appendChild(nameElement);
       plantItemDiv.appendChild(priceElement);
+
+      // When clicking on the products in landing page this will open up
+      plantItemDiv.addEventListener("click", () =>{
+        window.location.replace("https://medieinstitutet.se/utbildningar/front-end-developer/");
+      })
+
       // Append the wrapper div to the main container
       productpageDiv.appendChild(plantItemDiv);
     }
