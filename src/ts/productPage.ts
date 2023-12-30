db91d44d5bb6a6a894eb09142e3a30b2b5200b3e
Amanda Valencia <amandaavalencia@hotmail.com>
2023-12-30 19:50:50 +0100

    sessionStorage

--- src/ts/productPage.ts
+++ src/ts/productPage.ts
@@ -1,5 +1,6 @@
 import "./../scss/style.scss";
 import "./../scss/product.scss"
+import { Plant } from "./products";
 
 const addButton = document.getElementById("increment") as HTMLButtonElement;
 const removeButton = document.getElementById("decrement") as HTMLButtonElement;
@@ -21,19 +22,35 @@ removeButton.addEventListener("click", () => {
     }
 })
 
-let selectedPlantData = window.sessionStorage.getItem("selectedPlant");
+// let selectedPlantData = window.sessionStorage.getItem("selectedPlant");
+// let selectedPlant = null;
+// if (selectedPlantData !== null) {
+//     selectedPlant = JSON.parse(selectedPlantData) as Plant;
+// }
+// else {
+//     console.log("did not find selected plant in sessionStorage");
+// }
+// console.log(selectedPlant.plantName);
 
-let selectedPlant = null;
+let selectedPlantData = sessionStorage.getItem("selectedPlant");
+let selectedPlant: Plant | null = null;
+
 if (selectedPlantData !== null) {
-    selectedPlant = JSON.parse(selectedPlantData);
+    selectedPlant = JSON.parse(selectedPlantData) as Plant;
 }
 else {
     console.log("did not find selected plant in sessionStorage");
 }
 
-console.log(selectedPlant.plantName);
+if (selectedPlant !== null) {
+    console.log(selectedPlant.plantName);
+    clickedPlantInformation()
+}
+else {
+    console.log("selectedPlant is null");
+}
 
-
+function clickedPlantInformation (){
 const plantName = document.getElementById("plantName") as HTMLHeadElement;
 const productPlantImg = document.getElementById("productPlantImg") as HTMLImageElement;
 const plantDescription = document.getElementById("plantDescription") as HTMLParagraphElement;
@@ -44,12 +61,27 @@ const plantCareSpecifications = document.getElementById("plantCareSpecifications
 const priceOfPlant = document.getElementById("priceOfPlant") as HTMLParagraphElement;
 const nameInLatin = document.getElementById("nameInLatin") as HTMLParagraphElement;
 
-plantName.innerHTML = selectedPlant.plantName;
-productPlantImg.src = selectedPlant.image;
-plantDescription.innerHTML = selectedPlant.productInfo;
-plantCareSunlight.innerHTML = selectedPlant.needOfSunlight;
-plantCareWatering.innerHTML = selectedPlant.watering;
-plantCareSpecifications.innerHTML = selectedPlant.plantCareSpecifications;
-plantSpecifications.innerHTML = selectedPlant.plantSpecifications;
-priceOfPlant.innerHTML = selectedPlant.price +"kr inkl. moms";
-nameInLatin.innerHTML= selectedPlant.nameInLatin;
+plantName.innerHTML = selectedPlant!.plantName;
+productPlantImg.src = selectedPlant!.image;
+plantDescription.innerHTML = selectedPlant!.productInfo;
+plantCareSunlight.innerHTML = selectedPlant!.needOfSunlight;
+plantCareWatering.innerHTML = selectedPlant!.watering;
+plantCareSpecifications.innerHTML = selectedPlant!.plantCareSpecifications;
+plantSpecifications.innerHTML = selectedPlant!.plantSpecifications;
+priceOfPlant.innerHTML = selectedPlant!.price +"kr inkl. moms";
+nameInLatin.innerHTML= selectedPlant!.nameInLatin;
+}
+
+document.getElementById("addToCart")?.addEventListener("click",()=>{
+const productCard = document.getElementById("productCard") as HTMLDivElement;
+    const cartDiv = document.createElement("div");
+    const cartItemName = document.createElement("h3");
+    cartItemName.innerHTML = selectedPlant!.plantName;
+    console.log(selectedPlant!.plantName);
+
+    
+
+    cartDiv.appendChild(cartItemName);
+    productCard?.appendChild(cartDiv);
+})
+
