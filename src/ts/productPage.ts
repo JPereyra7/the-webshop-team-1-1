2a4d0d5aaa16354921d9e7e437a2ed8cb3a42b78
puuri <alexander.jagerlind@gmail.com>
2024-01-02 23:13:48 +0100

    Solved issues with add to cart functionality

--- src/ts/productPage.ts
+++ src/ts/productPage.ts
@@ -5,11 +5,17 @@ import "./../scss/product.scss"
 const addButton = document.getElementById("increment") as HTMLButtonElement;
 const removeButton = document.getElementById("decrement") as HTMLButtonElement;
 const numberOfPlant = document.getElementById("numberOfPlant") as HTMLDivElement;
+const addToCartButton = document.getElementById("addToCart") as HTMLButtonElement;
 let plantNumber = 1;
+
+// may need later
+// let incrementAmount: number = 0;
+// let decrementAmount: number = 0;
 
 addButton.addEventListener("click", () => {
     plantNumber += 1;
     numberOfPlant.innerHTML = plantNumber.toString();
+    return plantNumber;
 })
 
 removeButton.addEventListener("click", () => {
@@ -23,7 +29,6 @@ removeButton.addEventListener("click", () => {
 })
 
 let selectedPlantData = window.sessionStorage.getItem("selectedPlant");
-
 let selectedPlant = null;
 if (selectedPlantData !== null) {
     selectedPlant = JSON.parse(selectedPlantData);
@@ -32,8 +37,29 @@ else {
     console.log("did not find selected plant in sessionStorage");
 }
 
-console.log(selectedPlant.plantName);
+addToCartButton.addEventListener("click", () => {
+    Array.from({length:plantNumber}, () => addProduct(selectedPlant));
+})
 
+function addProduct(product) {
+    let exisingEntries = JSON.parse(localStorage.getItem("storedProducts") || '[]');
+    if (!exisingEntries.includes(product)) {
+      exisingEntries.push(product);
+      localStorage.setItem("storedProducts", JSON.stringify(exisingEntries));
+    } else {
+      console.log(product + "Product already in local storage");
+    }
+}
+
+// may need later
+// function removeProduct(product) {
+//     let exisingEntries = JSON.parse(localStorage.getItem("storedProducts") || '[]');
+//     exisingEntries.pop();
+//     localStorage.setItem("storedProducts", JSON.stringify(exisingEntries));
+// }
+
+// function updateProduct(product) {
+// }
 
 const plantName = document.getElementById("plantName") as HTMLHeadElement;
 const productPlantImg = document.getElementById("productPlantImg") as HTMLImageElement;
