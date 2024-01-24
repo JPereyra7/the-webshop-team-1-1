21cf4f639c1523fb09c6da0671c89d377ad5f043
Joel Pereyra <joelpereyra1993@gmail.com>
2024-01-24 20:47:09 +0100

    Fixed links through vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
-export default {
-    base: '/the-webshop-team-1-1/'
-
-    
-  };
+export default ({
+    base: '/the-webshop-team-1-1/',
+        plugins: [
+          svgr(),
+          react({
+            babel: {
+              plugins: [
+                [
+                  'babel-plugin-styled-components',
+                  {
+                    displayName: true,
+                    fileName: false,
+                  },
+                ],
+              ],
+            },
+          }),
+        ],
+      });
