db804262ba2b33071b3393e62818389291f92e9b
Joel Pereyra <joelpereyra1993@gmail.com>
2024-01-25 12:54:09 +0100

    configured vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
+import { resolve } from 'path';
 export default {
-    base: '/the-webshop-team-1-1/'
-
+    base: '/the-webshop-team-1-1/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                nested: resolve(__dirname, 'vaxter.html'),
+                nested2: resolve(__dirname, 'skotselrad.html'),
+                nested3: resolve(__dirname, 'productPage.html'),
+                nested4: resolve(__dirname, 'omOss.html'),
+                nested5: resolve(__dirname, 'kontaktaOss.html'),
+                nested6: resolve(__dirname, 'checkoutPage.html'),
+            }
+        }
+    }
     
   };
