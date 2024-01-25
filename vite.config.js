c3ce3a5154ec9634a34d8647323db61033bcab06
Joel Pereyra <joelpereyra1993@gmail.com>
2024-01-25 12:39:47 +0100

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,5 @@
-export default ({
-    base: '/the-webshop-team-1-1/',
-        plugins: [
-          svgr(),
-          react({
-            babel: {
-              plugins: [
-                [
-                  'babel-plugin-styled-components',
-                  {
-                    displayName: true,
-                    fileName: false,
-                  },
-                ],
-              ],
-            },
-          }),
-        ],
-      });
+export default {
+    base: '/the-webshop-team-1-1/'
+
+    
+  };
